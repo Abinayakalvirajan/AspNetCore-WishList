ea01aec755c59be9e505e34e28e6a872e76f0c2a
Abinaya <88155924+Abinayakalvirajan@users.noreply.github.com>
2022-07-08 17:37:50 +0530

    Update Startup.cs

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -1,19 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WishList.Data;
 
 namespace WishList
 {
-    public class Startup
-    {
-        // This method gets called by the runtime. Use this method to add services to the container.
+ public class Startup
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
@@ -26,18 +18,15 @@ namespace WishList
         {
             if (env.IsDevelopment())
             {
+              
                 app.UseDeveloperExceptionPage();
             }
             else
             {
                 app.UseExceptionHandler("/Home/Error");
             }
-            app.UseRouting();
 
-            app.UseEndpoints(endpoints => {
-                endpoints.MapDefaultControllerRoute();
-            });
-
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
